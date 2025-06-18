6aa79e4d6b2d82e5f85ce25bd95d5b6f8e1e8511
MDNSenevi <nilu.senevi@mds.ac.nz>
2025-06-19 11:43:49 +1200

    Add anonymous key

--- assets/js/anon-connection.js
+++ assets/js/anon-connection.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseURL = "https://kgnynmszwznrurzgrtdy.supabase.co";
-const supabaseKey = "somekey";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnbnlubXN6d3pucnVyemdydGR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NjEzNzMsImV4cCI6MjA2NTMzNzM3M30.WWGNsx0XI2vd48xJ5cjVyxmqfD_BeA50zaG7u5zoWJ0";
 
 export const supabase = createClient(supabaseURL, supabaseKey);
